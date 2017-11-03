df2e66e48b6715ce9f2cf38eb53359d08f35fd13
gerardo19 <leylin.esquivel@ucrso.info>
2017-11-03 10:37:17 -0600

    Inicio proyecto

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <ul>
+              <li>Incidentes</li>
+              <li>It Service request</li>
+              <li>Problemas</li>
+            </ul>
+          </nav>
+        </header>
+
+      </div>
+    );
+  }
+}
+
+export default App;
