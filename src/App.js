ed9871c4030057f1b04f323b8e1c8035684f7579
gerardo19 <leylin.esquivel@ucrso.info>
2017-11-17 11:30:22 -0600

    Estilos de incidentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
-import incidentes from './components/incidentes';
 import ListView from './components/listview';
 import ReactDOM from 'react-dom';
+import Incidentes from './components/incidentes';
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { RaisedButton } from 'material-ui';
 import Navbar from './components/navbar'
 import './App.css';
-class App extends Component {
+
+export default class App extends Component {
 
   render() {
     return (
       <div className="App">
         <MuiThemeProvider >
           <Navbar />
-          <ListView />
+          <Incidentes />
         </MuiThemeProvider>
+
       </div>
     );
   }
 }
-
-export default App;
--- src/components/incidentes.js
+++ src/components/incidentes.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
-import listview from './listview';
+import ListView from './listview';
+import '../styles/styles.css'
 
 export default class Incidentes extends Component {
-  render() {
-    return (
-      <div>
-
-      <listview />
-      
-        
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <div className="left-side">
+                    <ListView className="left-side" />
+                </div>
+                <div className="right-side">
+                    <ListView className="left-side" />
+                </div>
+            </div>
+        )
+    }
 }
